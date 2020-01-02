d36c7eade976ec52ae0a1514048202c1fd4b9005
BuildTools <unconfigured@null.spigotmc.org>
2020-01-02 22:17:33 +0700

    Add new function - AddEditCoffee

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sqlite3
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QDialog
 
 
 class MyWidget(QMainWindow):
@@ -11,6 +11,7 @@ class MyWidget(QMainWindow):
         self.con = sqlite3.connect("coffee.sqlite")
         self.show_from_request('Select * from coffee')
         self.edit_find.textChanged.connect(self.find_coffee)
+        self.pushButton.clicked.connect(self.open_add_edit)
 
     def show_from_request(self, req):
         self.tableWidget.clear()
@@ -34,6 +35,57 @@ class MyWidget(QMainWindow):
     def find_coffee(self):
         coffee = self.edit_find.text()
         self.show_from_request(f"Select * from coffee where variety_name = '{coffee}'")
+
+    def open_add_edit(self):
+        self.add_edit_wind = AddEdit(self.titles)
+        self.add_edit_wind.exec()
+        self.show_from_request(f"Select * from coffee")
+
+
+class AddEdit(QDialog):
+    def __init__(self, titles):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.tableWidget.setColumnCount(len(titles))
+        self.tableWidget.setHorizontalHeaderLabels(titles)
+        self.tableWidget.setRowCount(1)
+        self.lineEdit.textChanged.connect(self.find)
+        self.pushButton.clicked.connect(self.save)
+        self.id = None
+
+    def find(self):
+        for j in range(self.tableWidget.columnCount()):
+            self.tableWidget.setItem(0, j, QTableWidgetItem(''))
+        cur = self.con.cursor()
+        result = None
+        if self.lineEdit.text().isdigit():
+            result = cur.execute(
+                f'Select * from coffee where id = {self.lineEdit.text()}').fetchall()
+        if not result:
+            self.id = None
+            return
+
+        self.id = int(self.lineEdit.text())
+        for j, val in enumerate(result[0]):
+            self.tableWidget.setItem(0, j, QTableWidgetItem(str(val)))
+
+    def save(self):
+        values = []
+        for i in range(self.tableWidget.columnCount()):
+            values.append(self.tableWidget.item(0, i).text())
+
+        values = list(map(lambda x: x if x.isdigit() else f"'{x}'", values))
+
+        cur = self.con.cursor()
+        if self.id is None:
+            cur.execute(f"INSERT INTO coffee VALUES({', '.join(values)})")
+        else:
+            for i in range(len(values)):
+                values[i] = self.tableWidget.horizontalHeaderItem(i).text() + ' = ' + values[i]
+            cur.execute(f"UPDATE coffee SET {', '.join(values)} WHERE id = {self.id}")
+        self.con.commit()
+        self.close()
 
 
 app = QApplication(sys.argv)
